67f9efaca9643de87f38e1f6ceb07905100ae4b9
Ronakjain515 <Jain@810Jain@810>
2021-01-18 00:53:14 +0530

    login page complete with sessions

--- testApp/views.py
+++ testApp/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
-
+import json
+from django.http import JsonResponse
+from django.shortcuts import render, redirect
+from django.db.models import Q
+from .models import User
 # Create your views here.
 
 
@@ -8,7 +11,25 @@ def index(request):
 
 
 def login(request):
-    return render(request, "login.html")
+    message = ""
+    if request.method == "POST":
+        try:
+            email = request.POST.get("email")
+            password = request.POST.get("password")
+            isEmailExist = User.objects.all().filter(email=email).count()
+            if isEmailExist == 1:
+                isUserExist = User.objects.all().filter(Q(email=email) & Q(password=password)).count()
+                if isUserExist == 1:
+                    request.session["email"] = email
+                    request.session["password"] = password
+                    return redirect("register")
+                else:
+                    message = "Invalid Credentials..."
+            else:
+                message = "Email not Exists..."
+        except:
+            message = "Something went wrong..."
+    return render(request, "login.html", context={"message": message})
 
 
 def register(request):
